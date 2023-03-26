59eb97a1a4e8e81a0507cb94c113bb8ef5f53344
dlwnghd <alion0513@naver.com>
2023-03-26 23:41:48 +0900

    오류2...

--- src/repository/TokenService.js
+++ src/repository/TokenService.js
@@ -6,8 +6,7 @@ const TokenService = {
 	// get (token 가져오기)
 	async getAccessToken() {
 		// Access Token을 반환하는 비동기 함수를 사용합니다.
-		const token = await getTokenAsync() // 예시 코드, getTokenAsync()는 Access Token을 비동기적으로 반환하는 함수입니다.
-		return token
+		return `${process.env.REACT_APP_ACCESS_TOKEN}`
 	},
 }
 
